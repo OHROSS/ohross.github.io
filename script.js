24eaf19eaf9eb8eabafe5dabdce46a15e4abcd8c
Vasyl Kochura <vasylkochura@gmail.com>
2017-11-06 17:49:32 -0500

    MUSICMAPHTML

---
+++ script.js
@@ -0,0 +1,12 @@
+var headers = {
+    'User-Agent':       'Instrumentalist/1.0',
+    'Content-Type':     'application/json',
+    'username':         'MRLibrary',
+    'password':         'musicforlife'
+}
+
+//Discogs Response function
+  var url = "https://api.discogs.com/database/search?query=guitar&genre=rock&decade=1950&page=1&per_page=100&key=QFadPMPgXoKgieIaYcjN&secret=JcjaQHEjMHCxKLyxMamxjhptyPudjnsK";
+  $.ajax({url: url,
+
+  })
